daab201febe51d339db39651237511f4fdef4f6f
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-27 16:23:31 +0300

    Додає стилі до компонента Searchbar.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { StyledApp } from './AppStyled';
+import { Searchbar } from './Searchbar/Searchbar';
+
 export const App = () => {
+  // state:= {};
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledApp>
+      <Searchbar />
+    </StyledApp>
   );
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import {
+  StyleSearchbar,
+  StyleSearchForm,
+  StyleButton,
+  StyledButtonLable,
+  StyleSerchInput,
+} from './Searchbar.styled';
 
 export class Searchbar extends Component {
   state = {
     search: '',
   };
 
+  handleInputChange = evt => {
+    this.setState({ search: evt.target.value });
+  };
+
   render() {
-    return <></>;
+    const { search } = this.state;
+    console.log(this.state);
+    return (
+      <>
+        <StyleSearchbar>
+          <StyleSearchForm>
+            <StyleButton type="submit">
+              <StyledButtonLable>Search</StyledButtonLable>
+            </StyleButton>
+
+            <StyleSerchInput
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              onChange={this.handleInputChange}
+              value={search}
+            />
+          </StyleSearchForm>
+        </StyleSearchbar>
+      </>
+    );
   }
 }
