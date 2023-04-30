7054a34aea98ebd95805be7687d8c749c28b243b
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-30 17:27:03 +0300

    редагує початковий статус кнопки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ export class App extends Component {
     page: 1,
     isLoading: false,
     error: null,
-    showLoadMoreBtn: false,
+    showLoadMoreBtn: true,
   };
 
   async componentDidMount() {
